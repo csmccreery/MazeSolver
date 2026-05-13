65121b6e6ddc7aeec2f280ba4944835e2bb31ac3
csmccreery <cameron@mccreery.dev>
2026-05-13 00:36:12 -0400

    Due to my own confusion about what I had made the x and y coordinates the solver was getting stuck at the starting position. I have named the variables better and corrected the 'direction' logic. Now it seems to be failing to identify walls where it was doing so before. Curious

--- src/solver.py
+++ src/solver.py
@@ -5,76 +5,73 @@ from .cell import Cell
 class Solver:
     def __init__(self, maze: Maze) -> None:
         self.__maze: Maze = maze
-        self.__starting_position = (self.__maze.x1, self.__maze.y1)
-        self.__current_cell: Cell = self.__maze.cells[
-                self.__starting_position[0]][self.__starting_position[1]
-            ]
 
-    def __valid_cell(self, direction, i, j) -> bool:
+    def __valid_cell(self, direction, next_y, next_x) -> bool:
         valid_cell_position = (
-                i >= self.__maze.y1 and i <= self.__maze.num_rows and
-                j >= self.__maze.x1 and j <= self.__maze.num_cols
+                next_y >= self.__maze.y1 and next_y <= self.__maze.num_rows and
+                next_x >= self.__maze.x1 and next_x <= self.__maze.num_cols
             )
 
         if not valid_cell_position:
+            print(f"Position {next_y} outside bounds [{self.__maze.y1}, {self.__maze.num_rows}]")
+            print(f"Position {next_x} outside bounds [{self.__maze.x1}, {self.__maze.num_cols}]")
             return False
 
-        if direction == "up" and self.__maze.cells[i][j].has_bottom_wall:
+        if direction == "up" and self.__maze.cells[next_y][next_x].has_top_wall:
+            print(f"cell at position [{next_y}, {next_x}] has top wall")
             return False
 
-        if direction == "down" and self.__maze.cells[i][j].has_top_wall:
+        if direction == "down" and self.__maze.cells[next_y][next_x].has_bottom_wall:
+            print(f"cell at position [{next_y}, {next_x}] has bottom wall")
             return False
 
-        if direction == "left" and self.__maze.cells[i][j].has_right_wall:
+        if direction == "left" and self.__maze.cells[next_y][next_x].has_left_wall:
+            print(f"cell at position [{next_y}, {next_x}] has left wall")
             return False
 
-        if direction == "right" and self.__maze.cells[i][j].has_left_wall:
+        if direction == "right" and self.__maze.cells[next_y][next_x].has_right_wall:
+            print(f"cell at position [{next_y}, {next_x}] has right wall")
             return False
 
-        if self.__maze.cells[i][j].visited:
+        if self.__maze.cells[next_y][next_x].visited:
+            print(f"cell at position [{next_y}, {next_x}] has been visited")
             return False
 
         return True
 
-    def solve_dfs(self) -> bool:
+    def solve_dfs(self, current_cell=None) -> bool:
         self.__maze.animate(0.05)
-        start_y, start_x = self.__starting_position
-        self.__maze.cells[start_x][start_y].visited = True
+        if not current_cell:
+            current_cell = self.__maze.cells[self.__maze.x1][self.__maze.y1]
 
-        if self.__current_cell.target:
+        pos_y, pos_x = current_cell.location
+
+        if current_cell.target:
             return True
 
         directions = [
-                    ("up",
-                     self.__current_cell.location[0],
-                     self.__current_cell.location[1] + 1
-                     ),
-                    ("down",
-                     self.__current_cell.location[0],
-                     self.__current_cell.location[1] - 1
-                     ),
-                    ("left",
-                     self.__current_cell.location[0] + 1,
-                     self.__current_cell.location[1]
-                     ),
-                    ("right",
-                     self.__current_cell.location[0] - 1,
-                     self.__current_cell.location[1]
-                     ),
+                    ("down", pos_y + 1, pos_x),
+                    ("up", pos_y - 1, pos_x),
+                    ("right", pos_y, pos_x + 1),
+                    ("left", pos_y, pos_x - 1),
                 ]
 
-        for direction, i, j in directions:
-            if self.__valid_cell(direction, i, j):
-                next_cell = self.__maze.cells[i][j]
-                self.__current_cell.draw_move(next_cell)
-                next_move = self.solve_dfs()
+        print(f"Directions: {directions}")
+        for direction, next_y, next_x in directions:
+            print(f"Testing [{direction}] at position [{next_y}, {next_x}]")
+            if self.__valid_cell(direction, next_y, next_x):
+                print(f"Cell at position [{next_y}, {next_x}] valid.")
+                next_cell = self.__maze.cells[next_y][next_x]
+                current_cell.draw_move(next_cell)
+                next_move = self.solve_dfs(next_cell)
 
                 if next_move:
                     return True
+                    break
+                else:
+                    current_cell.draw_move(next_cell, True)
+        return False
 
-                self.__current_cell.draw_move(next_cell)
-                self.__current_cell = next_cell
-            
     def solve_djikstra(self) -> bool:
         pass
 
